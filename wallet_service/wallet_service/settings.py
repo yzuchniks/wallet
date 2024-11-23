aab7102e957170d55fd62bbfec1f25d1e9ffd18b
Sergey Morgunov <Bo-Jack-Bo@yandex.ru>
2024-11-23 04:13:45 +0400

    Наладил обработку исключений с неверным форматом UUID в запросе.

--- wallet_service/wallet_service/settings.py
+++ wallet_service/wallet_service/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'wallets.middleware.UUIDValidationMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
